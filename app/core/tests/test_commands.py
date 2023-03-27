0ee1061ceb275751ed82838ed558fb1afe812ffb
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-03-27 16:10:35 -0400

    Added checks

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -26,13 +26,12 @@ class CommandTests(SimpleTestCase):
     def test_wait_for_db_delay(self, patched_sleep, patched_check):
         """Test waiting for database when getting OperationalError"""
 
-        patched_check.side_effect = [Psycopg2Error] * 2 + \
-            [OperationalError] * 3 + [True]
-        
-        call_command('wait_for_db')
+        patched_check.side_effect = (
+            [Psycopg2Error] * 2 + [OperationalError] * 3 + [True]
+        )
+
+        call_command("wait_for_db")
 
         self.assertEqual(patched_check.call_count, 6)
 
-        patched_check.assert_called_with(databases=['default'])
-    
-
+        patched_check.assert_called_with(databases=["default"])
