e129ac196c283b6b621d033c4080d9dbc58d34be
naterocks49 <108183408+naterocks49@users.noreply.github.com>
2023-04-01 21:50:59 -0400

    UPDATES

--- app/recipe/tests/test_recipe_api.py
+++ app/recipe/tests/test_recipe_api.py
@@ -36,7 +36,7 @@ def detail_url(recipe_id):
 
 def image_upload_url(recipe_id):
     """Create and return an image upload URL."""
-    return reverse("recipe:recipe-upload_image", args=[recipe_id])
+    return reverse("recipe:recipe-upload-image", args=[recipe_id])
 
 
 def create_recipe(user, **params):
